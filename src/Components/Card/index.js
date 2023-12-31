2af2412cc07fa9e81eaf809ab5d9be59a557863f
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-12-31 19:19:30 +0700

    Update index.js

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { MapIcon } from '../../../assets';
 import "./card.css";
 
 const Card = (props) => {
@@ -11,8 +10,7 @@ const { id, nama, alamat, kabupaten, kategoris } = props;
       <span className="card-category position-absolute rounded fw-bold">{kategoris.nama}</span>
       <section className="card-body d-flex flex-column">
         <p className="card-cagar-title text-uppercase fs-5 fw-bold">{nama}</p>
-          <section className="city-container d-flex flex-row w-100 mt-auto">
-            {/* <img className="city-map" src={MapIcon} alt="Icon Map"></img> */}
+          <section className="city-container d-flex flex-row w-100 mt-auto">          
             <p className="card-city text-capitalize ml-1 mb-0">{kabupaten}</p>
           </section>
       </section>
