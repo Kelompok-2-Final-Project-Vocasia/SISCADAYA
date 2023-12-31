42cb25919d1f6e0558a04101b7217a932d29faf3
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-12-31 17:31:46 +0700

    Update Login.js

--- src/Components/User/Login.js
+++ src/Components/User/Login.js
@@ -12,27 +12,39 @@ function Login() {
 
   const [home, setHome] = useState(true);
   const handleLogin = async (event) => {
-      event.preventDefault()
-      const data = await axios.post("http://localhost:4000/login", user)
+    event.preventDefault();
+    try {
+      //fetch to database
+      const data = await axios.post("http://localhost:4000/login", user);
       console.log(data);
       console.log(user);
-      localStorage.setItem("token", data.data.token)
-      localStorage.setItem("user", data.data.email)
-      localStorage.setItem("isAdmin", data.data.isAdmin)
+      localStorage.setItem("token", data.data.token);
+      localStorage.setItem("user", data.data.email);
+      localStorage.setItem("isAdmin", data.data.isAdmin);
       if (data.data.isAdmin == 1) {
-          navigate("/admin/cagar-budaya")
-      }else{
-        navigate("/cagar-budaya")
+        navigate("/admin/cagar-budaya");
+      } else {
+        navigate("/cagar-budaya");
       }
-      localStorage.setItem("userId", data.data.user)
-      setUser({ email: "", password: "" })
-  }
+      localStorage.setItem("userId", data.data.user);
+      setUser({ email: "", password: "" });
+    } catch (error) {
+      // Tangani kesalahan saat login gagal
+      swal({
+        title: "Gagal!",
+        text: "Email atau password salah. Silakan coba lagi.",
+        icon: "error",
+        button: "Ok",
+      });
+      console.error("Login error:", error);
+    }
+  };
   return (
     <div>
       {home ? (
         <form onSubmit={handleLogin}>
           <h3>LogIn</h3>
-       
+//email
           <div className="form-group">
             <label>Email</label>
             <input
@@ -45,7 +57,7 @@ function Login() {
               onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
           </div>
-
+//password
           <div className="form-group">
             <label>Password</label>
             <input
@@ -58,14 +70,13 @@ function Login() {
               onChange={(e) => setUser({...user,password:e.target.value})}
             />
           </div>
-
+//submit button
           <button type="submit" className="buton mb-4">
             Login
           </button>
+//register
           Don't have an account? <a href="/register" className="mt-5">Register</a>
-
         </form>
-
       ) : (
         <Home />
       )}
