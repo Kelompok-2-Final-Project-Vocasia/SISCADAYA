9d70e5798d52d601f1f1a160370485c112838b58
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-12-31 17:17:41 +0700

    Update Footer.js

--- src/Components/Landing/Footer.js
+++ src/Components/Landing/Footer.js
@@ -1,17 +1,14 @@
 import React from "react";
-// import Logo from "../Assets/Logo.svg";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs"; //logo twitter
+import { SiLinkedin } from "react-icons/si"; //logo linkedin
+import { BsYoutube } from "react-icons/bs"; //logo youtube
+import { FaFacebookF } from "react-icons/fa"; //logo facebook
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
       <div className="footer-section-one">
-        {/* <div className="footer-logo-container">
-          <img src={Logo} alt="" />
-        </div> */}
+    //deklarasi logo
         <div className="footer-icons">
           <BsTwitter />
           <SiLinkedin />
