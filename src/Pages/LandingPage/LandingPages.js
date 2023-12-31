7b40f19482fd11a45bc12211b7c392c170f58d11
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-12-31 19:16:25 +0700

    Update LandingPages.js

--- src/Pages/LandingPage/LandingPages.js
+++ src/Pages/LandingPage/LandingPages.js
@@ -2,8 +2,6 @@ import "./style.css";
 import Home from "../../Components/Landing/Home"
 import About from "../../Components/Landing/About";
 import Work from "../../Components/Landing/Work";
-import Testimonial from "../../Components/Landing/Testimonial";
-// import Contact from "../../Components/Landing/Contact";
 import Footer from "../../Components/Landing/Footer";
 import Type from "../../Components/Landing/Type";
 
@@ -15,8 +13,6 @@ function LandingPages() {
       <About />
       <Work />
       <Type />
-      {/* <Testimonial /> */}
-      {/* <Contact /> */}
       <Footer />
     </div>
   );
